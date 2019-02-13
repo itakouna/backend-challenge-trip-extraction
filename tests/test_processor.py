c62e362efc7cc60958aa078e2ac43aaab601517d
Ibrahim takouna <ibrahim.takouna@deliveryhero.com>
2019-02-13 22:57:00 +0100

    Add test for processor/get_trip

--- tests/test_processor.py
+++ tests/test_processor.py
@@ -1,4 +1,4 @@
-from processor import WaypointListProcessor, Waypoint
+from processor import WaypointListProcessor, Waypoint, Trip
 import pytest
 
 
@@ -28,3 +28,67 @@ class TestProcessor():
     def test_car_trip_has_ended(self, points, expected):
         list_processor = WaypointListProcessor([])
         assert list_processor._car_trip_has_ended(points) == expected
+
+    def test_get_trip_for_with_non_stop_point(self):
+        waypoints = [Waypoint("2018-08-10T20:10:22Z", 54.54987, 12.41039),
+                     Waypoint("2018-08-10T20:15:22Z", 57.54987, 12.41039),
+                     Waypoint("2018-08-10T20:16:22Z", 58.54987, 12.41039),
+                     Waypoint("2018-08-10T20:17:22Z", 59.54987, 12.41039),
+                     Waypoint("2018-08-10T20:22:22Z", 59.64987, 12.41039)
+                     ]
+        list_processor = WaypointListProcessor(waypoints)
+        assert list_processor.get_trips() == [
+            Trip(
+                distance=567942.9577481565,
+                start=Waypoint(timestamp='2018-08-10T20:10:22Z',
+                               lat=54.54987, lng=12.41039),
+                end=Waypoint(timestamp='2018-08-10T20:22:22Z',
+                             lat=59.64987, lng=12.41039))]
+
+    def test_get_trip_with_two_trips(self):
+        waypoints = [
+            Waypoint("2018-08-10T20:04:22Z", 52.54987, 12.41039),
+            Waypoint("2018-08-10T20:06:22Z", 54.54987, 12.41039),
+            Waypoint("2018-08-10T20:08:22Z", 54.54987, 12.41039),
+            Waypoint("2018-08-10T20:10:22Z", 54.54987, 12.41039),
+            Waypoint("2018-08-10T20:15:22Z", 57.54987, 12.41039),
+            Waypoint("2018-08-10T20:16:22Z", 58.54987, 12.41039),
+            Waypoint("2018-08-10T20:17:22Z", 59.54987, 12.41039),
+            Waypoint("2018-08-10T20:22:22Z", 59.54987, 12.41039)
+        ]
+        list_processor = WaypointListProcessor(waypoints)
+        assert list_processor.get_trips() == [
+            Trip(distance=222593.12419555362,
+                 start=Waypoint(timestamp='2018-08-10T20:04:22Z',
+                                lat=52.54987, lng=12.41039),
+                 end=Waypoint(timestamp='2018-08-10T20:06:22Z',
+                              lat=54.54987, lng=12.41039)),
+            Trip(distance=556802.4117337333,
+                 start=Waypoint(timestamp='2018-08-10T20:10:22Z',
+                                lat=54.54987, lng=12.41039),
+                 end=Waypoint(timestamp='2018-08-10T20:17:22Z',
+                              lat=59.54987, lng=12.41039))
+        ]
+
+    def test_get_trip_distance_less_15m(self):
+        waypoints = [
+            Waypoint("2018-08-10T20:04:22Z", 52.54987, 12.41039),
+            Waypoint("2018-08-10T20:06:22Z", 52.54987, 12.41031),
+            Waypoint("2018-08-10T20:10:22Z", 52.54991, 12.41036)
+        ]
+        list_processor = WaypointListProcessor(waypoints)
+        assert list_processor.get_trips() == []
+
+    def test_get_trip_distance_more_15m(self):
+        waypoints = [
+            Waypoint("2018-08-10T20:04:22Z", 52.54987, 12.41039),
+            Waypoint("2018-08-10T20:06:22Z", 52.54989, 12.41039),
+            Waypoint("2018-08-10T20:10:22Z", 52.55998, 12.41039)
+        ]
+        list_processor = WaypointListProcessor(waypoints)
+        assert list_processor.get_trips() == [
+            Trip(distance=1125.0192831925704,
+                 start=Waypoint(timestamp='2018-08-10T20:04:22Z',
+                                lat=52.54987, lng=12.41039),
+                 end=Waypoint(timestamp='2018-08-10T20:10:22Z',
+                              lat=52.55998, lng=12.41039))]
