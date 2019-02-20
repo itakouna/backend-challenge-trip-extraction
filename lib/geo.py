57cf198f4af06175b1df927b81e6eeacd10725e5
Ibrahim takouna <ibrahim.takouna@deliveryhero.com>
2019-02-20 03:21:25 +0100

    Add type for compute_distance_in_meters

--- lib/geo.py
+++ lib/geo.py
@@ -1,6 +1,14 @@
 import abc
+from datetime import datetime
+from typing import NamedTuple
 from geopy.distance import distance
 from pyproj import Geod
+
+
+class Waypoint(NamedTuple):
+    timestamp: datetime
+    lat: float
+    lng: float
 
 
 class GeoLibrary(metaclass=abc.ABCMeta):
@@ -12,7 +20,8 @@ class GeoLibrary(metaclass=abc.ABCMeta):
         self._geo = geo_library
 
     @abc.abstractmethod
-    def compute_distance_in_meters(self, origin, destination):
+    def compute_distance_in_meters(self, origin: Waypoint,
+                                   destination: Waypoint) -> float:
         pass
 
 
@@ -21,7 +30,8 @@ class GeoAdapter(GeoLibrary):
     Adapt the interface of Geo library to the Target interface.
     """
 
-    def compute_distance_in_meters(self, origin, destination):
+    def compute_distance_in_meters(self, origin: Waypoint,
+                                   destination: Waypoint) -> float:
         return self._geo.compute_distance_in_meters(
             origin, destination)
 
@@ -31,7 +41,8 @@ class GeopyLibrary:
     Define an interface using Geopy library functions.
     """
 
-    def compute_distance_in_meters(self, origin, destination):
+    def compute_distance_in_meters(self, origin: Waypoint,
+                                   destination: Waypoint):
 
         return distance((origin.lat, origin.lng),
                         (destination.lat, destination.lng)).meters
@@ -42,7 +53,8 @@ class PyprojLibrary:
     Define an interface using Pyproj library functions.
     """
 
-    def compute_distance_in_meters(self, origin, destination):
+    def compute_distance_in_meters(self, origin: Waypoint,
+                                   destination: Waypoint) -> float:
         geod = Geod(ellps='WGS84')
         _, _, distance = geod.inv(
             origin.lng, origin.lat, destination.lng, destination.lat)
